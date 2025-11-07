a2b60d128b21d8d977201a672930342e2a4131bd
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-11-07 10:52:35 -0500

    insert std

--- F25Week9CodeFirstApproach/MainWindow.xaml.cs
+++ F25Week9CodeFirstApproach/MainWindow.xaml.cs
@@ -16,9 +16,41 @@ namespace F25Week9CodeFirstApproach
     /// </summary>
     public partial class MainWindow : Window
     {
+        SchoolContext db = new SchoolContext();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadStudents()
+        {
+            grdStudents.ItemsSource = db.Students.ToList();
+        }
+
+        private void btnLoadStudents_Click(object sender, RoutedEventArgs e)
+        {
+            LoadStudents();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cmbStandard.ItemsSource = db.Standards.ToList();
+            cmbStandard.DisplayMemberPath = "StandardName";
+            cmbStandard.SelectedValuePath = "StandardId";
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            Student std = new Student();
+            std.StudentName = txtName.Text;
+            std.StandardId = (int)cmbStandard.SelectedValue;
+
+            db.Students.Add(std);
+            db.SaveChanges();
+
+            LoadStudents();
+            MessageBox.Show("New student added");
         }
     }
 }
