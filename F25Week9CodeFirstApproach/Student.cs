7eb31ebd7f8f6fcf78764e86035406fdbb3ed8c4
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-11-07 09:44:48 -0500

    entities

---
+++ F25Week9CodeFirstApproach/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F25Week9CodeFirstApproach
+{
+    public class Student
+    {
+        // scalar properties
+        public int StudentId { get; set; }
+        public string? StudentName { get; set; }
+        public int? StandardId { get; set; }
+
+
+    }
+}
